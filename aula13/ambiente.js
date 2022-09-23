a5bd78451990d972fa215cfe4fe15f896ca547d3
Lucas Costa <luchtml5@gmail.com>
2022-09-23 14:56:24 -0300

    criei o ambiente de testes

---
+++ aula13/ambiente.js
@@ -0,0 +1,30 @@
+var c = 1
+do {
+    console.log (`Passo ${c}`)
+    c++
+} while (c <= 6) 
+console.log ('Acabou')
+
+
+
+
+/*var c = 1
+while (c <= 6) {
+    console.log (`Passo ${c}`)
+    c++
+} console.log ('Acabou')*/
+
+
+
+
+
+
+
+
+
+/* console.log ('Tudo bem')
+console.log ('Tudo bem')
+console.log ('Tudo bem')
+console.log ('Tudo bem')
+console.log ('Tudo bem')
+console.log ('Tudo bem') */
