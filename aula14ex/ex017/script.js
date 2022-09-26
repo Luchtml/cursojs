2f653599dd6e1615bc15e3d082a66e75beae26b9
Lucas Costa <luchtml5@gmail.com>
2022-09-26 17:20:18 -0300

    criei ex 017

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,25 @@
-alert ('Olá')
+function pegarNumero () {
+    var num = document.querySelector('input#txtn')
+    var tab = document.querySelector('select#seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número')
+    } else {
+      var n = Number(num.value)
+      var c = 1
+      tab.innerHTML = ""
+      for (c = 1; c <= 10; c++) {
+        var item = document.createElement('option')
+        item.text = `${n} x ${c} = ${n*c}`
+        item.value = `tab${c}`
+        tab.appendChild(item)
+      }
+      /*while (c <= 10) {
+        var item = document.createElement('option')
+        item.text = `${n} x ${c} = ${n*c}`
+        item.value = `tab${c}`
+        tab.appendChild(item)
+        c++
+      } */ 
+    }
+    
+}
