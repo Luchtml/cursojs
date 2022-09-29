744a1e639faffdce654aa8041fbe9bb284fc6355
Lucas Costa <luchtml5@gmail.com>
2022-09-29 17:25:08 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -4,5 +4,11 @@ num.push(1)
 num.sort()
 
 console.log(`${num}`)
-console.log (`O vetor tem ${num.length} posições`)
 console.log (`O primeiro valor do vetor é ${num[0]}`)
+console.log (`O vetor tem ${num.length} posições`)
+let pos = num.indexOf(9)
+if (pos == -1) {
+    console.log(`O valor não foi encontrado`)
+} else {
+console.log(`O valor 9 está na posição ${pos}`)
+}
