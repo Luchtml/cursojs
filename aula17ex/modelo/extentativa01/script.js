ecf96ac1a4f1ee5fb05c56dbfd8d45c5f3285834
Lucas Costa <luchtml5@gmail.com>
2022-10-03 16:20:17 -0300

    modifiquei o exercicio

--- aula17ex/modelo/extentativa01/script.js
+++ aula17ex/modelo/extentativa01/script.js
@@ -7,13 +7,16 @@ function adicionar() {
     let item = document.createElement('option')
     let num = document.querySelector('input#fnum')
     if (isNumero(num.value) && !inLista(num.value, valores)) {
+        item.text = `Valor ${num.value} adicionado`
         lista.appendChild(item)
         valores.push(Number(num.value))
-        item.text = `Valor ${num.value} adicionado`
-        info.innerHTML = 'Registrando Valores...'        
+        info.innerHTML = 'Registrando Valores...' 
+        res.innerHTML = ''       
     } else {
         window.alert('Numero ja encontrado ou inválido')
     }
+    num.value = ''
+    num.focus()
 }
 
 function isNumero(n) {
@@ -33,30 +36,53 @@ function inLista(n, l) {
 }
 
 function pegarRegistro() {
-    valores.sort
     let soma = 0
-    let itemRes = document.createElement('p')
-    let ultimo = valores[valores.length - 1]
+    let media = 0
+    let tot = valores.length
+    let maior = valores[0]
+    let menor = valores[0]
     if (valores.length != 0) {
-        for(let pos in valores) {        
-            res.appendChild(itemRes)
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores [pos] > maior) {
+                maior = valores[pos]
+            }
+            if (valores[pos] < menor) {
+                menor = valores[pos]
+            }        
+            /*res.appendChild(itemRes)
             itemRes.innerHTML +=
-             `<strong>${valores[pos]}</strong>, `
+             `<strong>${valores[pos]}</strong>, `*/
         }
-        for (let i=0; i < valores.length;i++){
-            soma += valores[i]
-        }
-        itemRes.innerHTML += `<br><br>Foram adicionados <strong>${valores.length}</strong> números`
+        media = soma / tot
+        mediaCon = parseInt(media)
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, foram adicionados ${tot} números!</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
+        rein.style.display = 'block'
+
+
+        /*itemRes.innerHTML += `<p>Foram adicionados <strong>${valores.length}</strong> números</p>`
         itemRes.innerHTML += 
-        `<br><br>A soma de todos os valores é <strong>${soma}</strong<br><br>`
+        `<p>A soma de todos os valores é <strong>${soma}</strong></p>`
         itemRes.innerHTML += 
-        `O maior valor adicionado é o valor <strong>${ultimo}</strong><br><br>`
+        `<p>O maior valor adicionado é o valor <strong>${ultimo}</strong></p>`
         itemRes.innerHTML += 
-            `O menor valor adicionado é o valor <strong>${valores.shift(0)}</strong><br><br>`
+            `<p>O menor valor adicionado é o valor <strong>${valores.shift(0)}</strong></p>`
         itemRes.innerHTML += 
-        `A média dos valores é: <strong>${soma / valores.length}</strong>`
+        `<p>A média dos valores é: <strong>${soma / valores.length}</strong></p>`*/
 
     } else {
         alert('Não há números registrados')
     }
+}
+
+function reiniciar() {
+    valores = [0]
+    lista.innerHTML = `<option>Adicione números acima</option>`
+    res.innerHTML = ''
+    rein.style.display = 'none'
 }
