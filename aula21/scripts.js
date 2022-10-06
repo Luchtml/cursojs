2685fb53ec35b26e2e2a48fe86eacb086f729110
Lucas Costa <luchtml5@gmail.com>
2022-10-06 11:03:33 -0300

    a

---
+++ aula21/scripts.js
@@ -0,0 +1,16 @@
+function exibir(num) {
+    console.log("A oepração resultou em: " + num)
+}
+
+function soma(a, b, callback) {
+    let op = a + b
+    callback(op)
+}
+
+function multiplicacao(a, b, cb){
+    let op = a * b
+    cb(op)
+}
+
+soma(2, 2, exibir)
+multiplicacao(2, 4, exibir)
---
+++ aula22/setTimeout-setInterval/scripts.js
@@ -0,0 +1,3 @@
+setTimeout(function()) {
+console.log('testando o setTimeout')
+}, 2000)
