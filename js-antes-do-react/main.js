603b5e8e789d2ca32fe315d05f73145c49503073
Lucas Costa <luchtml5@gmail.com>
2022-10-10 16:17:07 -0300

    a

--- js-antes-do-react/main.js
+++ js-antes-do-react/main.js
@@ -80,14 +80,161 @@
 //NÂO precisei descrever name = name e age = age, short Syntax resume tudo
 // document.body.innerText = JSON.stringify(user)
 
-const user = {
-  name: 'Lucas',
-  age: '26',
-  address: {
-    street: 'Rua teste',
-    number: '56',
-  },
-}
+
+//optional chaining
+
+// const user = {
+//   name: 'Lucas',
+//   age: '26',
+//   address: {
+//     street: 'Rua teste',
+//     number: '56',
+//     zip: {
+//       code: '8916000',
+//       city: 'Sao Jose dos Campos'
+//     },
+//     showFullAddress() {
+//       return 'ok'
+//     }
+//   },
+// }
 
 
-document.body.innerText = user.address.street
+// const key = 'state'
+
+
+//métodos de array
+
+// const array = [1, 2 , 3, 4, 5]
+// for(const i of array){
+//   document.body.innerText += i
+// }
+//FOrma de percorrer todo array
+
+//document.body.innerText = user.address[key]
+
+// const array = [1, 2 , 3, 4, 5]
+// // const novoArray = []
+
+// // array.forEach( item => {
+// //   novoArray.push(item * 2)
+// // })
+// //Outra forma de percorrer toda array
+
+// const novoArray = array.map(item=>{
+//   if(item % 2 === 0){
+//     return item * 10
+//   }
+  
+//     return item
+// })
+
+//map, filter, every, some, find, findIndex, reduce
+
+// const array = [1, 2 , 3, 4, 5,]
+// // const novoArray = array
+// // .filter(item => item % 2 === 0)
+// // .map(item => item * 10)
+// //const novoArray = array.filter(item => item % 2 !== 0)
+
+
+// const todosItensSaoNumeros = array.every(item => {
+//   return typeof item === 'number'
+// })
+// document.body.innerText = JSON.stringify(todosItensSaoNumeros)
+
+
+
+
+// const array = [1, 2 , 3, 4, 5]
+// const peloMenosUmItemNaoEUmNumero = array.some(item=> {
+//   return typeof item !== 'number'
+// })
+
+
+// const array = [1, 2 , 3, 4, 5]
+// const par = array.findIndex(item => item % 2 === 0)
+
+// const soma = array.reduce((acc, item) => {
+//   document.body.innerText += acc + ', ' + item + ' --- '
+
+//   return acc + item
+// }, 0)
+
+// document.body.innerText += JSON.stringify(soma)
+
+//template literals
+
+// const user = "Lucas"
+// const message = `Bem vindo, ${user ? user :  'Visitante'}`
+
+//Promises
+
+// .then/ .catch
+
+
+
+// somaDoisNumeros(1, 3)
+// .then(soma => {
+//   //deucerto
+//   document.body.innerText = soma
+// })
+// .catch(err => {
+//   //não deu certo
+//   console.log(err)
+// })
+
+
+// fetch('asdas')
+// .then (response => {
+//   return response.json()
+// })
+// .then(body => {
+//   console.log(body)
+// })
+// // .then (response => {
+// //   response.json().then(body => {
+// //     console.log(body)
+// //   })
+// //})
+// .catch(err=>{
+//   console.log(err)
+// })
+
+// .finally(()=> {
+//   console.log('deu')
+// })
+
+
+// async function buscaDadosNoGitHub() {
+//  try {
+//   const response = await fetch('https://api.github.com/users/luchtml')
+//   const body = await response.json()
+//   return body.name
+//  } catch (err) {
+//   console.log(err)
+//  } finally {
+//   console.log('deu')
+//  }
+//   //   fetch('asdas')
+// // .then (response => {
+// //   return response.json()
+// // })
+// // .then(body => {
+// //   console.log(body)
+// // })
+// // .catch(err=>{
+// //   console.log(err)
+// // })
+
+// // .finally(()=> {
+// //   console.log('deu')
+// // })
+
+// }
+
+// buscaDadosNoGitHub().then(name => {
+//   console.log(name)
+// })
+
+
---
+++ js-antes-do-react/main2.js
@@ -0,0 +1,2 @@
+import { soma, sub } from './lib/math'
+console.log(soma(1, 2))
