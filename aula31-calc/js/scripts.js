251647477b9c5e572b469223d495aef3a84c42d7
Lucas Costa <luchtml5@gmail.com>
2022-10-12 18:42:24 -0300

    a

---
+++ aula31-calc/js/scripts.js
@@ -0,0 +1,9 @@
+const operacaoAnterior = document.querySelector('.anterior')
+const operacaoAtual = document.querySelector('.atual')
+const botoes = document.querySelectorAll('.teclas')
+
+
+
+console.log(botoes)
+//eventos para adicionar e fazer funcionar
+
