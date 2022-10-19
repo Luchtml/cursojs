bccde3d901735cf11dd1c1567bade7e4f1cc7175
Lucas Costa <luchtml5@gmail.com>
2022-10-19 07:21:21 -0300

    a

---
+++ aula34 copy/js/scripts.js
@@ -0,0 +1,17 @@
+class Produto {
+    constructor() {
+        this.id = 0
+        this.nomeProduto = ''
+        this.valor = 0
+    }
+
+    adicionar() {
+        alert('Metodo adicionado')
+    }
+
+    excluir() {
+        alert('Item deletado')
+    }
+}
+
+let produto = new Produto ()
